3d7fc9c1620043fa9d5fe8803d645cb83f9e3f1c
colt-ain <ro7bps@gmail.com>
2017-03-09 20:45:32 +0300

    Changed file system

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# -mango-slider
 # mango-slider
-# mango-slider-1
+
+What is it. It's my fist hand-made slider on clearly javascript. You can use this in your pages any where and anytime.
+
+To install this item on the page:
+1. To add css styles in your css file or in html-file.
+Allmy styles was named like this: mng-....
+2. To add link to the file index.js to the end of the body tag.
+
+Added the side libs: fontawesome, bootstrap.
--- js/index.js
+++ js/index.js
@@ -1,14 +1,142 @@
-slide.onclick = function() {
-    var start = Date.now(); // сохранить время начала
 
-    var timer = setInterval(function() {
-        // вычислить сколько времени прошло из opts.duration
-        var timePassed = Date.now() - start;
-
-        slide.style.left = timePassed / 5 + 'px';
-
-        if (timePassed > 2000)  { clearInterval(timer);
+function Slider(name, number) {
+    this.name = name;
+    this.number = number;
+    this.left = "0px";
+    this.ShowNow = 0;
+    this.sliderOffset = [];
+    this.slideNumber = document.querySelector(".slider-container-" + this.number).querySelector(".slider").querySelector(".slider-slides").getElementsByTagName('img').length;
+    this.leftbutton =
+        document.querySelector(".slider-container-" + this.number).querySelector(".leftbutton");
+    this.rightbutton =  document.querySelector(".slider-container-" + this.number).querySelector(".rightbutton");
+    this.firstBub = document.querySelector(".slider-container-" + this.number).querySelector(".slider-controls").querySelector(".slider-controls-btn-1");
+    this.secondBub = document.querySelector(".slider-container-" + this.number).querySelector(".slider-controls").querySelector(".slider-controls-btn-2");
+    this.thirdBub = document.querySelector(".slider-container-" + this.number).querySelector(".slider-controls").querySelector(".slider-controls-btn-3");
+    this.imgWidth = function() {
+        // функция для подсчета ширины img в зависимости от количества слайдов
+        var that = this;
+        for (var i = 0; i < that.slideNumber; i++) {
+            document.querySelector(".slider-container-" + that.number).querySelector(".slider").querySelector(".slider-slides").querySelector(".slides" + ":nth-child(" + (i + 1) + ")").style.width = 100 / this.slideNumber + "%";
         }
-    },
-    20);
+    };
+    this.sliderWidth = function() {
+        // функция подсчета ширины всего блока с img от количества слайдов
+        document.querySelector(".slider-container-" + this.number).querySelector(".slider").querySelector(".slider-slides").style.width = this.slideNumber * 100 + "%";
+    };
+    this.slideMake = function() {
+        for (var i = 0; i < this.slideNumber; i++) {
+            this.sliderOffset.push(-i * 100 + '%');
+        }
+        console.log(this.sliderOffset);
+    };
+    this.slideLeft = function() {
+        var that = this;
+        this.leftbutton.onclick = function() {
+            --that.ShowNow;
+
+            if(that.ShowNow < 0) {
+                that.ShowNow = that.slideNumber - 1;
+            }
+            console.log(that.ShowNow);
+            that.lightBubs(that.ShowNow);
+            document.getElementById(that.name).style.left = that.sliderOffset[that.ShowNow];
+        }
+    };
+    this.slideRight = function() {
+        var that = this;
+        this.rightbutton.onclick = function() {
+            ++that.ShowNow;
+            if(that.ShowNow > that.slideNumber -1) {
+                that.ShowNow = 0;
+            }
+            console.log(that.ShowNow);
+            that.lightBubs(that.ShowNow);
+
+            document.getElementById(that.name).style.left = that.sliderOffset[that.ShowNow];
+        }
+    };
+
+    this.clickBubs = function() {
+        var that = this;
+        this.firstBub.onclick = function() {
+            if (that.ShowNow === 0) {
+                return;
+            } else {
+            that.ShowNow = 0;
+            document.getElementById(that.name).style.left = that.sliderOffset[that.ShowNow];
+            that.lightBubs(that.ShowNow);
+            }
+        };
+        this.secondBub.onclick = function() {
+            if (that.ShowNow === 1) {
+                return;
+            } else {
+            that.ShowNow = 1;
+            document.getElementById(that.name).style.left = that.sliderOffset[that.ShowNow];
+            that.lightBubs(that.ShowNow);
+            }
+        };
+        this.thirdBub.onclick = function() {
+            if (that.ShowNow === 2) {
+                return;
+            } else {
+                that.ShowNow = 2;
+            document.getElementById(that.name).style.left = that.sliderOffset[that.ShowNow];
+            that.lightBubs(that.ShowNow);
+            }
+        };
+    };
+    this.lightBubs = function() {
+        for (var i = 0; i < this.slideNumber; i++) {
+            if (this.ShowNow == 0) {
+                this.firstBub.classList.add("active");
+                this.secondBub.classList.remove("active");
+                this.thirdBub.classList.remove("active");
+            } else if (this.ShowNow == 1) {
+                this.firstBub.classList.remove("active");
+                this.secondBub.classList.add("active");
+                this.thirdBub.classList.remove("active");
+            } else if (this.ShowNow == 2) {
+                this.firstBub.classList.remove("active");
+                this.secondBub.classList.remove("active");
+                this.thirdBub.classList.add("active");
+            }
+        }
+    };
 }
+
+
+
+
+
+
+
+var slider1 = new Slider("slider1", 1);
+slider1.imgWidth();
+slider1.sliderWidth();
+slider1.slideMake();
+slider1.clickBubs();
+slider1.lightBubs();
+slider1.slideLeft();
+slider1.slideRight();
+slider1.lightBubs();
+
+var slider2 = new Slider("slider2", 2);
+slider2.imgWidth();
+slider2.sliderWidth();
+slider2.slideMake();
+slider2.clickBubs();
+slider2.lightBubs();
+slider2.slideLeft();
+slider2.slideRight();
+slider2.lightBubs();
+
+var slider3 = new Slider("slider3", 3);
+slider3.imgWidth();
+slider3.sliderWidth();
+slider3.slideMake();
+slider3.clickBubs();
+slider3.lightBubs();
+slider3.slideLeft();
+slider3.slideRight();
+slider3.lightBubs();
